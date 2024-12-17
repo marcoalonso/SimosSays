0ba64e45eef4dc0f079b3fed5c788f4aa04472e5
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2024-12-16 19:00:19 -0600

    Initial Commit

---
+++ SimonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimonDice
+//
+//  Created by Marco Alonso Rodriguez on 16/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimonDice/SimonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonDiceApp.swift
+//  SimonDice
+//
+//  Created by Marco Alonso Rodriguez on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SimonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
